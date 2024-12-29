1a7071d88eec60273a39b93ffffbea20e6efd0e3
Source-Code-007 <utsho926@gmail.com>
2024-12-29 21:33:17 +0600

    specialty section updated

--- src/app/(withCommonLayout)/specialty/_components/SpecialtySection.tsx
+++ src/app/(withCommonLayout)/specialty/_components/SpecialtySection.tsx
@@ -6,6 +6,7 @@ import { Tooltip } from "@nextui-org/tooltip";
 import Link from "next/link";
 import React from "react";
 
+
 const SpecialtySection = async () => {
   const specialty = await getSpecialty();
 
