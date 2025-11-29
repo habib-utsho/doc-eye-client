f2c6255ebe6c9fc0e6bf4243a8f538cd889f5e5f
Source-Code-007 <utsho926@gmail.com>
2025-11-29 10:01:48 +0600

    navbar dropdown hamburger nav items fix and profile dropdown added for mobile view

--- src/components/shared/navbar.tsx
+++ src/components/shared/navbar.tsx
@@ -79,7 +79,7 @@ export const Navbar = () => {
       </NavbarContent>
       <NavbarMenu>
         <div className="mx-4 mt-2 flex flex-col gap-2">
-          {siteConfig.navMenuItems.map((item, index) => {
+          {siteConfig.navItems.map((item, index) => {
             const active = isActive(item.href);
             return (
               <NavbarMenuItem key={`${item.href}-${index}`}>
@@ -98,6 +98,9 @@ export const Navbar = () => {
           })}
         </div>
       </NavbarMenu>
+      <NavbarItem className="flex md:hidden">
+        <NavbarProfileDropdown />
+      </NavbarItem>
     </NextUINavbar>
   );
 };
